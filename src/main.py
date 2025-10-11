a2471ec765a719b37f52549ce9b274c255aa2fd9
soi23476 <soikook4@gmail.com>
2025-10-11 11:57:18 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,32 @@
+import matplotlib.pyplot as plt
 
+# 1. 데이터 불러오기
+left_vals = []
+right_vals = []
+
+with open('data.txt', 'r') as f:
+    for line in f:
+        parts = line.strip().split(',')
+        if len(parts) == 2:
+            try:
+                left_vals.append(float(parts[0]))
+                right_vals.append(float(parts[1]))
+            except:
+                continue
+
+# 2. 그래프 그리기
+plt.figure(figsize=(10,5))
+plt.plot(left_vals, label='Left sensor(L)', color='blue')
+plt.plot(right_vals, label='Right sensor(R)', color='red')
+plt.title('result')
+plt.xlabel('number')
+plt.ylabel('distances(cm)')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+wear_left = max(left_vals) - min(left_vals)
+wear_right = max(right_vals) - min(right_vals)
+
+print(f"left interval: {wear_left:.2f} cm")
+print(f"right interval: {wear_right:.2f} cm")
